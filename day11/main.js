eab1b1034e20c7aa0a6073e4ed678457df39e654
hridaygoswami <epikhriday@gmail.com>
2023-11-15 23:13:18 +0530

    Adding files from Day1 to Day15

---
+++ day11/main.js
@@ -0,0 +1,12 @@
+let obj = [
+    {
+        "fname":"Hriday"
+    },
+    {
+        "fname":"Dev"
+    }
+]
+
+obj.map((o)=>{
+    console.log(o.fname)
+})
---
+++ day3/main.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a+b
+
+# add(2,3)
+
+a = 10
+print(a, add(2,3))
